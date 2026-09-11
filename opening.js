cd581b7e29b04136872cab00c61171cd07c3653d
Alaiya Stevens <alaiyastevens@my.tridenttech.edu>
2026-09-11 13:11:53 -0400

    Add opening animation on enter button click

--- opening.js
+++ opening.js
@@ -1,1 +1,16 @@
+const enterButton = document.getElementById("enterButton");
+const magazine = document.querySelector(".magazine");
 
+enterButton.addEventListener("click", function () {
+
+    enterButton.disabled = true;
+
+    magazine.classList.add("opening");
+
+    setTimeout(function () {
+
+        window.location.href = "index.html";
+
+    }, 2300);
+
+});
